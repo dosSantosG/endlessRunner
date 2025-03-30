fe5f18ced4e767a6aa728a0086bdf146936b858c
Gabriel <gabriel.ferreira.santos.2001@gmail.com>
2025-03-29 21:06:15 -0300

    Adding first assests

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import pygame
+from code.game import Game
 
-pygame.init()
-window = pygame.display.set_mode(size=(1024, 640))
+game = Game()
+game.run()
 
-while True:
-    # Check for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    pass
